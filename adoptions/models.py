6cbdab100e2248add04ff64fb7f604b472221e0a
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-05-02 14:59:18 +0800

    migrate model and load pet data from csv

--- adoptions/models.py
+++ adoptions/models.py
@@ -10,7 +10,7 @@ class Pet(models.Model):
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
-    vacinnations = models.ManyToManyField('Vaccine', blank=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
